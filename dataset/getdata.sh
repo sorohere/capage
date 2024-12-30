3ed2117385ffda4a27e056f0af6982715fcb38a7
Saurabh Kushwaha <1ms22cs126@msrit.edu>
2024-12-31 04:05:56 +0530

    getdata

---
+++ dataset/getdata.sh
@@ -0,0 +1,13 @@
+chmod +w .
+
+wget "https://github.com/sorohere/flickr-dataset/releases/download/v0.1.0/flickr30k-dataset-part_00"
+wget "https://github.com/sorohere/flickr-dataset/releases/download/v0.1.0/flickr30k-dataset-part_01"
+wget "https://github.com/sorohere/flickr-dataset/releases/download/v0.1.0/flickr30k-dataset-part_02"
+
+cat flickr30k-dataset-part_* > flickr30k-dataset.zip
+unzip -q flickr30k-dataset.zip 
+
+rm flickr30k-dataset-part_00 flickr30k-dataset-part_01 flickr30k-dataset-part_02
+rm flickr30k-dataset.zip
+
+echo "Downloaded Flickr30k dataset successfully."
