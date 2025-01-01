d1d32d8543a6fbb4d2feb99ad657c893e992c570
Saurabh Kushwaha <1ms22cs126@msrit.edu>
2025-01-01 10:53:52 +0530

    updated

--- deploy/model/getweights.sh
+++ deploy/model/getweights.sh
@@ -1,8 +1,12 @@
+cd deploy/model
+
 wget "https://github.com/sorohere/flickr-dataset/releases/download/v0.2.0/model.zip"
 wget "https://github.com/sorohere/flickr-dataset/releases/download/v0.2.0/vocab.zip"
 
-unzip model.zip -d ./model
-unzip vocab.zip -d ./model
+unzip model.zip 
+unzip vocab.zip 
 
 rm model.zip
 rm vocab.zip
+
+rm -rf __MACOSX
